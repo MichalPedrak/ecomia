4c806fe00c39f253cd414474a77db933bacd88d8
MichalPedrak <michalpedrak07@gmail.com>
2024-04-25 00:28:05 +0200

    all changes

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+
+import vue from '@vitejs/plugin-vue'
+import laravel from "laravel-vite-plugin";
+
 
 export default defineConfig({
+
     plugins: [
+        vue(),
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+
     ],
 });
