a3459f7cb6f808749cc807b378cd9359a36c93f4
MichalPedrak <michalpedrak07@gmail.com>
2024-04-27 14:03:47 +0200

    Data collect test

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,24 @@ use App\Models\DataCollector;
 
 Artisan::command('collect data', function () {
     DataCollector::collect();
-})->purpose('Delete recent users')->hourly();
+
+
+
+})->purpose('Collect DATA')->everyMinute();
+
+
+
+function wh_log($log_msg)
+{
+    $log_filename = "log";
+    if (!file_exists($log_filename)) 
+    {
+        // create directory/folder uploads.
+        mkdir($log_filename, 0777, true);
+    }
+    $log_file_data = $log_filename.'/log_' . date('d-M-Y') . '.log';
+    // if you don't add `FILE_APPEND`, the file will be erased each time you add a log
+    file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);
+} 
+// call to function
+wh_log("this is my log message");
